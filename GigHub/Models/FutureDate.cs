3fc7cae98d15077fda2f657c850ebbfad3462808
delphiy <delphix2012@gmail.com>
2018-10-09 15:48:56 +0100

    Implement validation.

---
+++ GigHub/Models/FutureDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class FutureDate:ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            var idValid = DateTime.TryParseExact(Convert.ToString(value), 
+                "d MMM yyyy", 
+                CultureInfo.CurrentCulture, 
+                DateTimeStyles.None, 
+                out dateTime);
+            return (idValid && dateTime > DateTime.Now);
+        }
+    }
+}
